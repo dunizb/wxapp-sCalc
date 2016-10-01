15577146817262ee6dc64b4d39b1299c065d4b01
dunizb <dunizb@outlook.com>
2016-10-01 11:05:27 +0800

    完善历史记录功能

--- pages/history/history.js
+++ pages/history/history.js
@@ -1,10 +1,12 @@
 Page({
   data:{
     // text:"这是一个页面"
+    logs:[]
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
-    console.log("页面初始化");
+    var logs = wx.getStorageSync('calclogs');
+    this.setData({"logs":logs});
   },
   onReady:function(){
     // 页面渲染完成
